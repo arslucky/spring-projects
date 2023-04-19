7ff0e7f93216b263839051096d21558f3a302f65
arslucky <arsibragimov@gmail.com>
2023-04-19 07:41:34 +0300

    set environment variables in upper case

--- oauth2/build.docker.sh
+++ oauth2/build.docker.sh
@@ -6,20 +6,20 @@ function prop {
     grep "${1}=" ./commons/src/main/resources/default.properties|cut -d'=' -f2
 }
 
-log_dir=logs
-log_host_dir=/mnt/c/$log_dir
-log_container_dir=/app/$log_dir
-network=oauth2
-auth=auth-server
-auth_port=$(prop 'auth.port')
-eureka=eureka-server
-eureka_port=$(prop 'eureka.port')
-kafka=kafka
-kafka_port=$(prop 'kafka.port')
-zoo=zookeeper
-zoo_port=$(prop 'zoo.port')
-conf=config-server
-conf_port=$(prop 'config.server.port')
+LOG_DIR=logs
+LOG_HOST_DIR=/mnt/c/$LOG_DIR
+LOG_CONTAINER_DIR=/app/$LOG_DIR
+NETWORK=oauth2
+AUTH=auth-server
+AUTH_PORT=$(prop 'auth.port')
+EUREKA=eureka-server
+EUREKA_PORT=$(prop 'eureka.port')
+KAFKA=kafka
+KAFKA_PORT=$(prop 'kafka.port')
+ZOO=zookeeper
+ZOO_PORT=$(prop 'zoo.port')
+CONF=config-server
+CONF_PORT=$(prop 'config.server.port')
 MYSQL=mysql
 MYSQL_PORT=$(prop 'mysql.port')
 MYSQL_USER=root
@@ -29,21 +29,21 @@ MONGO=mongo
 MONGO_PORT=$(prop 'mongo.port')
 MONGO_ROOT_PASSWORD=password
 
-echo 'conf_port='$conf_port
-echo 'log_dir='$log_dir
-echo 'log_host_dir='$log_host_dir
-echo 'log_container_dir='$log_container_dir
-echo 'network='$network
-echo 'auth='$auth
-echo 'auth_port='$auth_port
-echo 'eureka='$eureka
-echo 'eureka_port='$eureka_port
-echo 'kafka='$kafka
-echo 'kafka_port='$kafka_port
-echo 'zoo='$zoo
-echo 'zoo_port='$zoo_port
-echo 'conf='$conf
-echo 'conf_port='$conf_port
+echo 'CONF_PORT='$CONF_PORT
+echo 'LOG_DIR='$LOG_DIR
+echo 'LOG_HOST_DIR='$LOG_HOST_DIR
+echo 'LOG_CONTAINER_DIR='$LOG_CONTAINER_DIR
+echo 'NETWORK='$NETWORK
+echo 'AUTH='$AUTH
+echo 'AUTH_PORT='$AUTH_PORT
+echo 'EUREKA='$EUREKA
+echo 'EUREKA_PORT='$EUREKA_PORT
+echo 'KAFKA='$KAFKA
+echo 'KAFKA_PORT='$KAFKA_PORT
+echo 'ZOO='$ZOO
+echo 'ZOO_PORT='$ZOO_PORT
+echo 'CONF='$CONF
+echo 'CONF_PORT='$CONF_PORT
 echo 'MYSQL='$MYSQL
 echo 'MYSQL_PORT='$MYSQL_PORT
 echo 'MYSQL_USER='$MYSQL_USER
@@ -55,31 +55,31 @@ echo 'MONGO_ROOT_PASSWORD='$MONGO_ROOT_PASSWORD
 
 ##########################################################################################
 
-docker network create $network
+docker network create $NETWORK
 
 ##########################################################################################
-docker rm -f $zoo
-docker run -dp $zoo_port:$zoo_port --network $network --network-alias $zoo --hostname $zoo --name $zoo \
+docker rm -f $ZOO
+docker run -dp $ZOO_PORT:$ZOO_PORT --network $NETWORK --network-alias $ZOO --hostname $ZOO --name $ZOO \
     -e ALLOW_ANONYMOUS_LOGIN=yes \
-    -e ZOO_PORT_NUMBER=$zoo_port \
+    -e ZOO_PORT_NUMBER=$ZOO_PORT \
     bitnami/zookeeper:3.7.1
 
-docker rm -f $kafka
-docker run -dp $kafka_port:$kafka_port --network $network --network-alias $kafka --hostname $kafka --name $kafka \
+docker rm -f $KAFKA
+docker run -dp $KAFKA_PORT:$KAFKA_PORT --network $NETWORK --network-alias $KAFKA --hostname $KAFKA --name $KAFKA \
     -e ALLOW_PLAINTEXT_LISTENER=yes \
-    -e KAFKA_CFG_ZOOKEEPER_CONNECT=$zoo:$zoo_port \
-    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:$kafka_port \
-    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://:$kafka_port \
+    -e KAFKA_CFG_ZOOKEEPER_CONNECT=$ZOO:$ZOO_PORT \
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:$KAFKA_PORT \
+    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://:$KAFKA_PORT \
     bitnami/kafka:3.3.2
 
 echo "creating topic log"
 
-docker exec -it $kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic log --bootstrap-server $kafka:$kafka_port
+docker exec -it $KAFKA /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic log --bootstrap-server $KAFKA:$KAFKA_PORT
 
 ##########################################################################################
 
 docker rm -f $MYSQL
-docker run -dp $MYSQL_PORT:$MYSQL_PORT --network $network --network-alias $MYSQL --hostname $MYSQL --name $MYSQL \
+docker run -dp $MYSQL_PORT:$MYSQL_PORT --network $NETWORK --network-alias $MYSQL --hostname $MYSQL --name $MYSQL \
     -e MYSQL_TCP_PORT=$MYSQL_PORT \
     -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD \
     mysql:8.0.32
@@ -96,7 +96,7 @@ docker exec -i $MYSQL sh -c 'exec mysql -u'$MYSQL_USER' -p'$MYSQL_PASSWORD' --po
 ##########################################################################################
 
 docker rm -f $MONGO
-docker run -dp $MONGO_PORT:$MONGO_PORT --network $network --network-alias $MONGO --hostname $MONGO --name $MONGO \
+docker run -dp $MONGO_PORT:$MONGO_PORT --network $NETWORK --network-alias $MONGO --hostname $MONGO --name $MONGO \
     -e MONGODB_ROOT_PASSWORD=$MONGO_ROOT_PASSWORD \
     -e MONGODB_PORT_NUMBER=$MONGO_PORT \
     bitnami/mongodb:6.0.5
@@ -112,233 +112,233 @@ done
 
 ##########################################################################################
 
-docker rm -f $conf
-docker rmi -f $conf
-docker build -t $conf ./config-server
-docker run -dp $conf_port:$conf_port --network $network --network-alias $conf --name $conf \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
-    $conf
+docker rm -f $CONF
+docker rmi -f $CONF
+docker build -t $CONF ./config-server
+docker run -dp $CONF_PORT:$CONF_PORT --network $NETWORK --network-alias $CONF --name $CONF \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
+    $CONF
 
-log=log-server
+LOG=log-server
 
-docker rm -f $log
-docker rmi -f $log
-docker build -t $log ./log-server
-docker run -d --network $network --network-alias $log --name $log \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
-    $log
+docker rm -f $LOG
+docker rmi -f $LOG
+docker build -t $LOG ./log-server
+docker run -d --network $NETWORK --network-alias $LOG --name $LOG \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
+    $LOG
 
-docker rm -f $eureka
-docker rmi -f $eureka
-docker build -t $eureka ./eureka-server
-docker run -dp $eureka_port:$eureka_port --network $network --network-alias $eureka --name $eureka \
-    -v $log_host_dir:$log_container_dir \
-    -e port=$eureka_port \
-    -e log.dir=$log_dir \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
-    $eureka
+docker rm -f $EUREKA
+docker rmi -f $EUREKA
+docker build -t $EUREKA ./eureka-server
+docker run -dp $EUREKA_PORT:$EUREKA_PORT --network $NETWORK --network-alias $EUREKA --name $EUREKA \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e port=$EUREKA_PORT \
+    -e log.dir=$LOG_DIR \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
+    $EUREKA
 
-docker rm -f $auth
-docker rmi -f $auth
-docker build -t $auth ./authorization-server
-docker run -dp $auth_port:$auth_port --network $network --network-alias $auth --name $auth \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e host=$auth \
-    -e port=$auth_port \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
-    $auth
+docker rm -f $AUTH
+docker rmi -f $AUTH
+docker build -t $AUTH ./authorization-server
+docker run -dp $AUTH_PORT:$AUTH_PORT --network $NETWORK --network-alias $AUTH --name $AUTH \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e host=$AUTH \
+    -e port=$AUTH_PORT \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
+    $AUTH
 
-gtw=gateway
+GTW=gateway
 
-docker rm -f $gtw
-docker rmi -f $gtw
-docker build -t $gtw ./gateway-zuul
-docker run -dp 8080:8080 --network $network --network-alias $gtw --name $gtw \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e auth.host=$auth \
-    -e auth.port=$auth_port \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
-    $gtw
+docker rm -f $GTW
+docker rmi -f $GTW
+docker build -t $GTW ./gateway-zuul
+docker run -dp 8080:8080 --network $NETWORK --network-alias $GTW --name $GTW \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e auth.host=$AUTH \
+    -e auth.port=$AUTH_PORT \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
+    $GTW
 
-ui1=ui1
-ui2=ui2
+UI1=ui1
+UI2=ui2
 
-docker rm -f $ui1
-docker rm -f $ui2
+docker rm -f $UI1
+docker rm -f $UI2
 docker rmi -f ui
 docker build -t ui ./ui
-docker run -d --network $network --name $ui1 --network-alias $ui1 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e auth.host=$auth \
-    -e auth.port=$auth_port \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$ui1 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $UI1 --network-alias $UI1 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e auth.host=$AUTH \
+    -e auth.port=$AUTH_PORT \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$UI1 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     ui
 
-docker run -d --network $network --name $ui2 --network-alias $ui2 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e auth.host=$auth \
-    -e auth.port=$auth_port \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$ui2 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $UI2 --network-alias $UI2 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e auth.host=$AUTH \
+    -e auth.port=$AUTH_PORT \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$UI2 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     ui
 
-res1=resource1
-res2=resource2
+RES1=resource1
+RES2=resource2
 
-docker rm -f $res1
-docker rm -f $res2
+docker rm -f $RES1
+docker rm -f $RES2
 docker rmi -f resource
 docker build -t resource ./resource
-docker run -d --network $network --name $res1 --network-alias $res1 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e auth.host=$auth \
-    -e auth.port=$auth_port \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$res1 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $RES1 --network-alias $RES1 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e auth.host=$AUTH \
+    -e auth.port=$AUTH_PORT \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$RES1 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     resource
 
-docker run -d --network $network --name $res2 --network-alias $res2 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e auth.host=$auth \
-    -e auth.port=$auth_port \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$res2 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $RES2 --network-alias $RES2 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e auth.host=$AUTH \
+    -e auth.port=$AUTH_PORT \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$RES2 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     resource
 
 ##########################################################################################
-cust1=customer-service1
-cust2=customer-service2
+CUST1=customer-service1
+CUST2=customer-service2
 
-docker rm -f $cust1
-docker rm -f $cust2
+docker rm -f $CUST1
+docker rm -f $CUST2
 docker rmi -f customer-service
 docker build -t customer-service ./customer-service
 
-docker run -d --network $network --name $cust1 --network-alias $cust1 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$cust1 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e zoo.host=$zoo \
-    -e zoo.port=$zoo_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $CUST1 --network-alias $CUST1 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$CUST1 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e zoo.host=$ZOO \
+    -e zoo.port=$ZOO_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     -e mysql.host=$MYSQL \
     -e mysql.port=$MYSQL_PORT \
     -e mysql.db=$MYSQL_DB \
     customer-service
 
-docker run -d --network $network --name $cust2 --network-alias $cust2 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$cust2 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $CUST2 --network-alias $CUST2 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$CUST2 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     -e mysql.host=$MYSQL \
     -e mysql.port=$MYSQL_PORT \
     -e mysql.db=$MYSQL_DB \
     customer-service
 
 ##########################################################################################
-ord1=order-service1
-ord2=order-service2
+ORD1=order-service1
+ORD2=order-service2
 
-docker rm -f $ord1
-docker rm -f $ord2
+docker rm -f $ORD1
+docker rm -f $ORD2
 docker rmi -f order-service
 docker build -t order-service ./order-service
 
-docker run -d --network $network --name $ord1 --network-alias $ord1 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$ord1 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $ORD1 --network-alias $ORD1 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$ORD1 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     -e mongo.host=$MONGO \
     -e mongo.port=$MONGO_PORT \
     order-service
 
-docker run -d --network $network --name $ord2 --network-alias $ord2 \
-    -v $log_host_dir:$log_container_dir \
-    -e log.dir=$log_dir \
-    -e eureka.host=$eureka \
-    -e eureka.port=$eureka_port \
-    -e eureka.instance.hostname=$ord2 \
-    -e kafka.host=$kafka \
-    -e kafka.port=$kafka_port \
-    -e config.server.host=$conf \
-    -e config.server.port=$conf_port \
+docker run -d --network $NETWORK --name $ORD2 --network-alias $ORD2 \
+    -v $LOG_HOST_DIR:$LOG_CONTAINER_DIR \
+    -e log.dir=$LOG_DIR \
+    -e eureka.host=$EUREKA \
+    -e eureka.port=$EUREKA_PORT \
+    -e eureka.instance.hostname=$ORD2 \
+    -e kafka.host=$KAFKA \
+    -e kafka.port=$KAFKA_PORT \
+    -e config.server.host=$CONF \
+    -e config.server.port=$CONF_PORT \
     -e mongo.host=$MONGO \
     -e mongo.port=$MONGO_PORT \
     order-service
